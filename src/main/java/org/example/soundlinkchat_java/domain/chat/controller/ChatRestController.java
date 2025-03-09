9bf2951b71194a2e52d0447a93858e7cd0fe5ad0
moon-kun-woong <plm303333@gmail.com>
2025-03-09 22:01:27 +0900

    [fix] ChatRestCotroller 레이어 정리2

--- src/main/java/org/example/soundlinkchat_java/domain/chat/controller/ChatRestController.java
+++ src/main/java/org/example/soundlinkchat_java/domain/chat/controller/ChatRestController.java
@@ -2,11 +2,8 @@ package org.example.soundlinkchat_java.domain.chat.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.example.soundlinkchat_java.domain.chat.dto.ChatDto;
 import org.example.soundlinkchat_java.domain.chat.dto.ChatResponseDto;
 import org.example.soundlinkchat_java.domain.chat.service.ChatService;
-import org.example.soundlinkchat_java.global.auth.JwtProvider;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,14 +15,13 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:63342")
 public class ChatRestController {
     private final ChatService chatService;
-    private final JwtProvider jwtProvider;
 
     @GetMapping("/history/{chatRoomId}")
     public ResponseEntity<List<ChatResponseDto>> getChatHistory(
             @PathVariable String chatRoomId,
             HttpServletRequest request
     ) {
-        return chatService.getChatHistoryByRoomId(chatRoomId, request);
+        return chatService.getChatHistoryResponse(chatRoomId, request);
     }
 }
 
