ff007318e12a39681007c0ded64cae75b7dc4bc0
moon-kun-woong <plm303333@gmail.com>
2025-03-10 19:37:13 +0900

    [fix] 컨슈머 리턴 시 isMyMessage = false 반환.

--- src/main/java/org/example/soundlinkchat_java/domain/chat/ChatConsumer.java
+++ src/main/java/org/example/soundlinkchat_java/domain/chat/ChatConsumer.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.soundlinkchat_java.domain.chat.dto.ChatDto;
+import org.example.soundlinkchat_java.domain.chat.dto.ChatResponseDto;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
@@ -27,7 +28,15 @@ public class ChatConsumer {
         try {
             ChatDto dto = objectMapper.readValue(message, ChatDto.class);
             if (dto.chatRoomId() != null) {
-                simpMessagingTemplate.convertAndSend("/queue/chat-" + dto.chatRoomId(), dto);
+                ChatResponseDto responseDto = new ChatResponseDto(
+                        dto.chatRoomId(),
+                        dto.fromUserId(),
+                        dto.message(),
+                        dto.createdAt(),
+                        false
+                );
+
+                simpMessagingTemplate.convertAndSend("/queue/chat-" + dto.chatRoomId(), responseDto);
             }
         } catch (Exception e) {
             log.error("[ChatConsumer] Failed: {}", e.getMessage());
